40f3688e30d8944925983333e990e448fde55929
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-12-08 20:36:55 -0300

    Criando o projeto

---
+++ src/test/java/br/com/mayconinforgames/med/voll/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconinforgames.med.voll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
